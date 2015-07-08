58ef88a7eb7cdadfee3c704dd6817d3431cc4b72
Mikel Eizagirre <meizagirre@irontec.com>
2015-07-08 13:26:42 +0200

    added sip.js files and binary file

--- ios/Config.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class SessionConfig {
-    var isInitiator: Bool
-    
-    init(data: AnyObject) {
-        self.isInitiator = data.objectForKey("isInitiator") as! Bool
-    }
-}
